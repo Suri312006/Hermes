0f561f580ba0afd10e83c3a981d86c17c5d5e406
suri312006 <suri312006@gmail.com>
2025-02-05 22:28:29 -0800

    pathoram

--- sparta/src/structures/messagestore.rs
+++ sparta/src/structures/messagestore.rs
@@ -1,8 +1,13 @@
 use color_eyre::eyre::{Context, Result};
-use oram::{Address, BlockSize, BlockValue, DefaultOram};
+use oram::{
+    path_oram::{DEFAULT_BLOCKS_PER_BUCKET, DEFAULT_RECURSION_CUTOFF, DEFAULT_STASH_OVERFLOW_SIZE},
+    Address, BlockSize, BlockValue, BucketSize, DefaultOram, OramBlock, PathOram, RecursionCutoff,
+    StashSize,
+};
 
 const BLOCK_SIZE: BlockSize = 64;
 const DB_SIZE: Address = 64;
+const BUCKET_SIZE: BucketSize = DEFAULT_BLOCKS_PER_BUCKET;
 
 /**
     The Message Store is implemented as an ORAM and stores queue nodes,
@@ -12,7 +17,7 @@ const DB_SIZE: Address = 64;
 
 **/
 pub struct MessageStore {
-    inner: DefaultOram<BlockValue<64>>,
+    inner: PathOram<BlockValue<64>, 4, 64>,
 }
 
 //TODO: implemented as an ORAM
@@ -20,10 +25,21 @@ impl MessageStore {
     pub fn setup() -> Result<Self> {
         let mut rng = rand::rngs::OsRng;
 
-        let oram = DefaultOram::<BlockValue<BLOCK_SIZE>>::new(DB_SIZE, &mut rng)
-            .with_context(|| "Failure when trying to initalize ORAM for Message Store.")?;
+        // let oram = DefaultOram::<BlockValue<BLOCK_SIZE>>::new(DB_SIZE, &mut rng)
+        //     .with_context(|| "Failure when trying to initalize ORAM for Message Store.")?;
 
-        Ok(Self { inner: oram })
+        let stash_size = StashSize::from(40_u16);
+
+        let path_oram =
+            PathOram::<BlockValue<BLOCK_SIZE>, BUCKET_SIZE, BLOCK_SIZE>::new_with_parameters(
+                DB_SIZE,
+                &mut rng,
+                DEFAULT_STASH_OVERFLOW_SIZE,
+                DEFAULT_RECURSION_CUTOFF,
+            )
+            .with_context(|| "Failure when trying to initalize PathORAM for Message Store.")?;
+
+        Ok(Self { inner: path_oram })
     }
 
     /**
