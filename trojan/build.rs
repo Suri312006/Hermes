6a6492681fe776427e2f99af0acb8314deecad05
suri312006 <suri312006@gmail.com>
2025-03-19 00:49:13 -0700

    added trojan

---
+++ trojan/build.rs
@@ -0,0 +1,12 @@
+fn main() {
+    tonic_build::configure()
+        .build_client(true)
+        // .protoc_arg("--experimental_allow_proto3_optional")
+        .use_arc_self(true)
+        // this is where our proto files are
+        .compile_protos(
+            &["../proto/message.proto", "../proto/user.proto"],
+            &["../proto"],
+        )
+        .unwrap_or_else(|e| panic!("unable to compile proto's due to {e:?}"))
+}
