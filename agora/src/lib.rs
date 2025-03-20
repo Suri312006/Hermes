60c4667c99f101242d0c3caffa2428fa51d354cf
suri312006 <suri312006@gmail.com>
2025-03-19 21:48:12 -0700

    pleaseeee

--- agora/src/lib.rs
+++ agora/src/lib.rs
@@ -20,7 +20,7 @@ pub const MSG_SIZE: usize =
     MSG_STORE_BLOCK_SIZE - (2 * MSG_ADDRESS_SIZE) as usize - MSG_RECIPIENT_SIZE as usize;
 
 /// The port that sparta will bind to.
-pub const TROJAN_BIND_ADDR: &str = "0.0.0.0:50051";
+pub const TROJAN_BIND_ADDR: &str = "0.0.0.0";
 
 pub const TROJAN_PORT: &str = "50051";
 
